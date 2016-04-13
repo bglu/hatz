7d66f60e0d3db1236033f4c8fc9ca03edb615efc
bglu <benjamin.glusa@online.de>
2016-04-13 19:08:38 +0200

    set datasource to locale mysql db

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -19,7 +19,11 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:mysql://192.168.178.39:3306/hatz"
+            username = "test"
+            password = "test"
+            driverClassName = "com.mysql.jdbc.Driver"
+            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
         }
     }
     test {
